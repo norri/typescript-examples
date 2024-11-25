613efe1304e93d7fca507b4dd8331cf60a636774
Eero Norri <eero.norri@nitor.com>
2024-11-25 21:41:28 +0200

    remove original hono example

--- hono/jest.config.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  preset: "ts-jest",
-  testEnvironment: "node",
-  testMatch: ["**/src/**/*.test.ts"],
-  collectCoverage: true,
-  coverageDirectory: "coverage",
-  verbose: true,
-};
